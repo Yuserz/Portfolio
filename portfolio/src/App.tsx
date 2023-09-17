3cd8648a9fc41b5e579bc3c1a340821e9afdfa3f
Yusri <yusripawaki2@gmail.com>
2023-09-17 15:45:19 +0800

    chore: minor fix and code modification

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-const Home: React.FC = () => <div>Hello world!</div>;
+import Home from "./pages/Home";
 
 const App: React.FC = () => (
   <Router>
