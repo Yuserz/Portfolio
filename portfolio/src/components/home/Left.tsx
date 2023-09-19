8264d33ba4715f02f1a6474c94bc24aff9c5c54d
Yusri <yusripawaki2@gmail.com>
2023-09-19 20:47:58 +0800

    fix: caption element issue

--- portfolio/src/components/home/Left.tsx
+++ portfolio/src/components/home/Left.tsx
@@ -7,16 +7,16 @@ export default function Left() {
     <>
       <h1 className="my-name">Hi, I’m YUSRI,</h1>
       <h2 className="title">FullStack Developer</h2>
-      <caption className="caption">
+      <div className="caption">
         I’m passionate about coding and problem-solving. My experience spans
         across a diverse tech stack and my eagerness to learn and adopt new
         technologies is ceaseless. I strive to create seamless user experiences
         with robust backend functionality.
-      </caption>
-      <caption className="caption">
+      </div>
+      <div className="caption">
         Feel free to explore my portfolio to see the projects I’ve worked on.
         Let’s code the future together!
-      </caption>
+      </div>
       <div className="contact-container flex items-center sm:self-center">
         <button className="contact-btn text-[16px]">
           Get in touch <img className="w-8 h-4 mt-[2px]" src={icon} />
--- portfolio/src/components/stacks/TeckStack.tsx
+++ portfolio/src/components/stacks/TeckStack.tsx
@@ -9,10 +9,12 @@ interface TechStackProps {
 const TechStack: React.FC<TechStackProps> = ({ image, title, caption }) => {
   return (
     <div className="card">
-      <img className="max-w-20 w-16 h-20 max-h-16" src={image} />
-      <div>
-        <h3 className="stack-title font-medium ">{title}</h3>
-        <caption className="caption whitespace-nowrap">{caption}</caption>
+      <div className="flex gap-4 w-full xs:justify-center sm:justify-center">
+        <img className="card-image p-1" src={image} />
+        <div className="self-start">
+          <h3 className="stack-title font-medium ">{title}</h3>
+          <div className="caption ">{caption}</div>
+        </div>
       </div>
     </div>
   );
