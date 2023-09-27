27dae6cb65a71ea1ece503940b4fab875cbb573c
Yusri <yusripawaki2@gmail.com>
2023-09-28 00:11:56 +0800

    feat: add footer

---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import * as icons from "../assets/icons";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="flex flex-col gap-4  mt-10 w-full h-[130px] bg-white-0 items-center justify-center">
+      <div className="flex flex-row gap-4 ">
+        <img className="footer-icon " src={icons.github} />
+        <img className="footer-icon " src={icons.facebook} />
+        <img className="footer-icon " src={icons.linkedin} />
+      </div>
+      <div className="text opacity-50 text-caption">
+        © 2023 | All rights reserved
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
