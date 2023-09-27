396e6a4a5aab50a8a3dcc9a2816b31420cb0e68e
Yusri <yusripawaki2@gmail.com>
2023-09-28 00:12:56 +0800

    chore: add new data

--- portfolio/src/components/stacks/TeckStack.tsx
+++ portfolio/src/components/stacks/TeckStack.tsx
@@ -1,22 +1,89 @@
 import * as React from "react";
+import * as icons from "../../assets/icons";
 
-interface TechStackProps {
-  image: string;
+interface Option {
+  id: number;
   title: string;
   caption: string;
+  icon: string;
 }
 
-const TechStack: React.FC<TechStackProps> = ({ image, title, caption }) => {
+const options: Option[] = [
+  {
+    id: 1,
+    title: "HTML5",
+    icon: icons.html,
+    caption: "HTML is the standard markup language for Web pages.",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    icon: icons.css,
+    caption: "Allows you to create great-looking web pages.",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    icon: icons.js,
+    caption: "A scripting or programming language.",
+  },
+  {
+    id: 4,
+    title: "ReactJS",
+    icon: icons.react,
+    caption: "A JS library that lets you put components together.",
+  },
+  {
+    id: 5,
+    title: "Tailwind",
+    icon: icons.tailwind,
+    caption: "Tailwind CSS is a utility-first CSS framework.",
+  },
+  {
+    id: 6,
+    title: "Firebase",
+    icon: icons.firebase,
+    caption: "Helps developers to build their apps faster. ",
+  },
+  {
+    id: 7,
+    title: "NodeJS",
+    icon: icons.nodejs,
+    caption: " Node.js is an open source server environment.",
+  },
+  {
+    id: 8,
+    title: "MongoDB",
+    icon: icons.mongodb,
+    caption: "Stores data in flexible, JSON-like documents.",
+  },
+  {
+    id: 9,
+    title: "React Native",
+    icon: icons.react,
+    caption: "Combines the best parts of native development with React.",
+  },
+];
+
+const TechStack: React.FC = () => {
   return (
-    <div className="card">
-      <div className="card-content">
-        <img className="card-image p-1" src={image} />
-        <div className="self-start">
-          <h3 className="stack-title font-medium ">{title}</h3>
-          <div className="caption ">{caption}</div>
+    <>
+      {options.map((option: Option, index) => (
+        <div key={index} className="card">
+          <div className="card-content">
+            <img className="card-image p-1" src={option.icon} />
+            <div className="self-start">
+              <h3 className="stack-title text-h4 font-medium text-dark-1">
+                {option.title}
+              </h3>
+              <div className="caption text-caption text-dark-1">
+                {option.caption}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
