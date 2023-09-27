d76bf89e0cd579834e1fdc11b8c965f9a0f4f5d9
Yusri <yusripawaki2@gmail.com>
2023-09-28 00:14:24 +0800

    style: add new assets

--- portfolio/src/assets/icons/index.ts
+++ portfolio/src/assets/icons/index.ts
@@ -6,3 +6,13 @@ export { default as tailwind } from "../icons/tailwind.svg";
 export { default as firebase } from "../icons/firebase.svg";
 export { default as nodejs } from "../icons/nodejs.svg";
 export { default as mongodb } from "../icons/mongodb.svg";
+
+export { default as mongodb2 } from "../icons/mongodb2.png";
+export { default as node2 } from "../icons/node2.png";
+export { default as react2 } from "../icons/react2.png";
+export { default as tailwind2 } from "../icons/tailwind2.png";
+export { default as firebase2 } from "../icons/firebase2.png";
+
+export { default as github } from "../icons/github.svg";
+export { default as facebook } from "../icons/facebook.svg";
+export { default as linkedin } from "../icons/linkedin.svg";
