d6e70fce84fa5cdc2fa5a15050f93a0e180fb3e1
Yusri <yusripawaki2@gmail.com>
2023-10-09 08:22:06 +0800

    feat: make social link working

--- portfolio/src/components/home/Left.tsx
+++ portfolio/src/components/home/Left.tsx
@@ -20,10 +20,18 @@ const item = {
 };
 
 const socialArray = [
-  { id: 1, icon: icons.github, path: "" },
-  { id: 2, icon: icons.facebook, path: "" },
-  { id: 3, icon: icons.twitter, path: "" },
-  { id: 4, icon: icons.linkedin, path: "" },
+  { id: 1, icon: icons.github, path: "https://github.com/Yuserz" },
+  {
+    id: 2,
+    icon: icons.facebook,
+    path: "https://www.facebook.com/YusriNaBroke/",
+  },
+  { id: 3, icon: icons.twitter, path: "https://twitter.com/Yusri56473275" },
+  {
+    id: 4,
+    icon: icons.linkedin,
+    path: "https://www.linkedin.com/in/yusri-caloyloy-b19217204/",
+  },
 ];
 
 export default function Left() {
@@ -68,11 +76,18 @@ export default function Left() {
 
         <motion.div className="flex gap-2">
           {socialArray.map((item) => (
-            <img
-              key={item.id}
-              className="max-w-10 max-h-10 w-full rounded-full opacity-90 hover:opacity-100 hover:scale-125 transition-all duration-300 cursor-pointer"
-              src={item.icon}
-            />
+            <button
+              onClick={() => {
+                const url = item.path;
+                window.open(url, "_blank");
+              }}
+            >
+              <img
+                key={item.id}
+                className="max-w-10 max-h-10 w-full rounded-full opacity-90 hover:opacity-100 hover:scale-125 transition-all duration-300 cursor-pointer"
+                src={item.icon}
+              />
+            </button>
           ))}
         </motion.div>
       </motion.div>
